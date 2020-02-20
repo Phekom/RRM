6f48a97704fe2b56db63e2c3004cd7d879aae9dc
Shaun McDonald <Shaun.McDonald@xisystems.co.za>
2020-02-20 15:35:00 +0200

    feature(RRM-Network): check for internet connectivity.

--- app/src/main/java/za/co/xisystems/itis_rrm/data/network/NetworkConnectionInterceptor.kt
+++ app/src/main/java/za/co/xisystems/itis_rrm/data/network/NetworkConnectionInterceptor.kt
@@ -7,20 +7,30 @@ import android.os.Build
 import androidx.annotation.RequiresApi
 import okhttp3.Interceptor
 import okhttp3.Response
+import za.co.xisystems.itis_rrm.ui.mainview.create.new_job_utils.Constants
+import za.co.xisystems.itis_rrm.utils.NoConnectivityException
 import za.co.xisystems.itis_rrm.utils.NoInternetException
+import java.io.IOException
+import java.net.HttpURLConnection
+import java.net.URL
+
 /**
  * Created by Francis Mahlava on 2019/10/18.
  */
 class NetworkConnectionInterceptor(
     context: Context
 ) : Interceptor {
-
+    private val testConnection = "https://itisqa.nra.co.za/ITISServicesMobile"
     private val applicationContext = context.applicationContext
 
     @RequiresApi(Build.VERSION_CODES.M)
     override fun intercept(chain: Interceptor.Chain): Response {
         if (!isInternetAvailable())
             throw NoInternetException("Make sure you have an active data connection")
+
+        if (!isHostReachable())
+            throw NoConnectivityException("Server unreachable, make sure you have data loaded.")
+
         return chain.proceed(chain.request())
     }
 
@@ -38,6 +48,23 @@ class NetworkConnectionInterceptor(
                 }
             }
         }
+        return result
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun isHostReachable(): Boolean {
+        var result: Boolean
+        try {
+            val connection = URL(testConnection).openConnection() as HttpURLConnection
+            connection.setRequestProperty("User-Agent", "Test")
+            connection.setRequestProperty("Connection", "close")
+            connection.connectTimeout = 1000
+            connection.connect()
+            result = (connection.responseCode == 200)
+        } catch (e: IOException) {
+            result = false
+        }
+
         return result
     }
 
--- app/src/main/java/za/co/xisystems/itis_rrm/utils/Exceptions.kt
+++ app/src/main/java/za/co/xisystems/itis_rrm/utils/Exceptions.kt
@@ -6,3 +6,4 @@ import java.io.IOException
  */
 class ApiException(message: String) : IOException(message)
 class NoInternetException(message: String) : IOException(message)
+class NoConnectivityException(message: String) : IOException(message)
