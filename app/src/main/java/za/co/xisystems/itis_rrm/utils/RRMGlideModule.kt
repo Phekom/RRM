0c6a048d85f7df2dbb216d12a3a4a24bacbf3b80
Shaun McDonald <shaun.mcdonald@xisystems.co.za>
2020-03-05 08:23:53 +0200

    chore(RRM_MvvM): add back missing Glide class
    Signed-off-by: Shaun McDonald <Shaun.McDonald@xisystems.co.za>

--- app/src/main/java/za/co/xisystems/itis_rrm/utils/RRMGlideModule.kt
+++ app/src/main/java/za/co/xisystems/itis_rrm/utils/RRMGlideModule.kt
@@ -5,6 +5,7 @@ import com.bumptech.glide.module.AppGlideModule
 
 /**
  * Created by Francis Mahlava on 2020/01/03.
+ * The new version of Glide (4.11) was not generating the GlideModule
  */
 
 @GlideModule
