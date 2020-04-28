3f327bfc920097e34c691d85f8593e7bf0585d12
Shaun McDonald <shaun.mcdonald@xisystems.co.za>
2020-04-28 11:07:16 +0200

    fix(network): NetworkConnectionInterceptor compatible with Android version 21 (Lollipop)
    Signed-off-by: Shaun McDonald <Shaun.McDonald@xisystems.co.za>

--- app/src/main/java/za/co/xisystems/itis_rrm/data/network/NetworkConnectionInterceptor.kt
+++ app/src/main/java/za/co/xisystems/itis_rrm/data/network/NetworkConnectionInterceptor.kt
@@ -4,11 +4,11 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Build
-import androidx.annotation.RequiresApi
 import okhttp3.Interceptor
 import okhttp3.Response
-import za.co.xisystems.itis_rrm.utils.NoConnectivityException
-import za.co.xisystems.itis_rrm.utils.NoInternetException
+import za.co.xisystems.itis_rrm.custom.errors.NoConnectivityException
+import za.co.xisystems.itis_rrm.custom.errors.NoInternetException
+import za.co.xisystems.itis_rrm.custom.errors.ServiceHostUnreachableException
 import java.io.IOException
 import java.net.InetAddress
 import java.net.InetSocketAddress
@@ -24,35 +24,55 @@ class NetworkConnectionInterceptor(
     private val serviceURL = "itisqa.nra.co.za"
     private val applicationContext = context.applicationContext
 
-    @RequiresApi(Build.VERSION_CODES.M)
+
     override fun intercept(chain: Interceptor.Chain): Response {
         if (!isInternetAvailable())
-            throw NoInternetException("Make sure you have an active data connection")
+            throw NoInternetException("Please ensure you have an active data connection")
 
         if (!isHostAvailable(host = testConnection, port = 443, timeout = 1000))
-            throw NoConnectivityException("Network appears to be down, please try again later.")
+            throw NoConnectivityException(
+                "Network appears to be down, please try again later."
+            )
 
         if (!isHostAvailable(host = serviceURL, port = 443, timeout = 1000)) {
-            throw NoConnectivityException("Service Host for RRM is down, please try again later.")
+            throw ServiceHostUnreachableException(
+                "Service Host for RRM is down, please try again later."
+            )
         }
 
         return chain.proceed(chain.request())
     }
 
-    @RequiresApi(Build.VERSION_CODES.M)
+
     private fun isInternetAvailable(): Boolean {
         var result = false
         val connectivityManager =
             applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
-        connectivityManager?.let {
-            it.getNetworkCapabilities(connectivityManager.activeNetwork)?.apply {
-                result = when {
-                    hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-                    hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-                    else -> false
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            connectivityManager?.let {
+                it.getNetworkCapabilities(connectivityManager.activeNetwork)?.apply {
+                    result = when {
+                        hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                        hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                        else -> false
+                    }
+                }
+            }
+        } else {
+            // For Android versions older than Marshmallow, this will work.
+            connectivityManager?.run {
+                @Suppress("DEPRECATION")
+                activeNetworkInfo?.run {
+                    result = when (type) {
+                        ConnectivityManager.TYPE_WIFI -> true
+                        ConnectivityManager.TYPE_MOBILE -> true
+                        else -> false
+                    }
                 }
             }
         }
+
         return result
     }
 
