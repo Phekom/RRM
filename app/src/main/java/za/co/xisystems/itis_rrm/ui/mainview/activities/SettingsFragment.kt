b7110051ced494aa6d9081d1b1ed668a6ff5b2ea
Shaun McDonald <shaun.mcdonald@xisystems.co.za>
2021-02-10 14:37:09 +0200

    MOBILEXI-1353 - Creation & Measurement Fixes
    Signed-off-by: Shaun McDonald <Shaun.McDonald@xisystems.co.za>

--- app/src/main/java/za/co/xisystems/itis_rrm/ui/mainview/activities/SettingsFragment.kt
+++ app/src/main/java/za/co/xisystems/itis_rrm/ui/mainview/activities/SettingsFragment.kt
@@ -5,10 +5,7 @@ import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreferenceCompat
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import za.co.xisystems.itis_rrm.R
-import za.co.xisystems.itis_rrm.utils.Coroutines
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
@@ -16,26 +13,23 @@ class SettingsFragment : PreferenceFragmentCompat() {
         savedInstanceState: Bundle?,
         rootKey: String?
     ) {
-        Coroutines.io {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
             val isChecked = false
-            withContext(Dispatchers.Main.immediate) {
-                val myTheme =
-                    findPreference<SwitchPreferenceCompat>(SettingsActivity.HOME)
-                myTheme!!.onPreferenceChangeListener =
-                    Preference.OnPreferenceChangeListener { _, newValue ->
-                        if (newValue == !isChecked) {
 
-                            (activity as SettingsActivity?)?.delegate?.localNightMode =
-                                AppCompatDelegate.MODE_NIGHT_YES
-                        } else {
+            val myTheme =
+                findPreference<SwitchPreferenceCompat>(SettingsActivity.HOME)
+            myTheme!!.onPreferenceChangeListener =
+                Preference.OnPreferenceChangeListener { _, newValue ->
+                    if (newValue == !isChecked) {
 
-                            (activity as SettingsActivity?)!!.delegate.localNightMode =
-                                AppCompatDelegate.MODE_NIGHT_NO
-                        }
-                        true
+                        (activity as SettingsActivity?)?.delegate?.localNightMode =
+                            AppCompatDelegate.MODE_NIGHT_YES
+                    } else {
+
+                        (activity as SettingsActivity?)!!.delegate.localNightMode =
+                            AppCompatDelegate.MODE_NIGHT_NO
                     }
-            }
-        }
+                    true
+                }
     }
 }
