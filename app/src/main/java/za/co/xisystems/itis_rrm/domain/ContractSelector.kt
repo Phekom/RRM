8b26ee55ee2c37edb6ed296fec329acb2251fa16
Shaun McDonald <shaun.mcdonald@xisystems.co.za>
2021-09-22 12:10:48 +0200

    🗃️  MOBILEXI-1882 Lightweight domain objects for faster selections
    Signed-off-by: Shaun McDonald <shaun.mcdonald@xisystems.co.za>
    
    Took 3 minutes

--- app/src/main/java/za/co/xisystems/itis_rrm/domain/ContractSelector.kt
+++ app/src/main/java/za/co/xisystems/itis_rrm/domain/ContractSelector.kt
@@ -4,8 +4,37 @@
 
 package za.co.xisystems.itis_rrm.domain
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class ContractSelector(
     val contractId: String,
     val contractNo: String?,
     val shortDescr: String?
-)
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString()!!,
+        parcel.readString(),
+        parcel.readString()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(contractId)
+        parcel.writeString(contractNo)
+        parcel.writeString(shortDescr)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<ContractSelector> {
+        override fun createFromParcel(parcel: Parcel): ContractSelector {
+            return ContractSelector(parcel)
+        }
+
+        override fun newArray(size: Int): Array<ContractSelector?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
--- app/src/main/java/za/co/xisystems/itis_rrm/domain/JobInfo.kt
+++
@@ -1,9 +0,0 @@
-package za.co.xisystems.itis_rrm.domain
-
-data class JobInfo(
-    var jobId: String,
-    var description: String,
-    var startKm: Double,
-    var endKm: Double,
-    var routeSection: String
-)
