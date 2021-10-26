3e835b9d28f4af0a961d5d3b069c21f3f3bea44d
Shaun McDonald <shaun.mcdonald@xisystems.co.za>
2021-10-26 17:09:11 +0200

    ✨ MOBILEXI-1965 Updated notifications and exception for sign-offs
    Signed-off-by: Shaun McDonald <shaun.mcdonald@xisystems.co.za>
    
    Took 1 hour 33 minutes

--- app/src/main/java/za/co/xisystems/itis_rrm/custom/notifications/ColorToast.kt
+++ app/src/main/java/za/co/xisystems/itis_rrm/custom/notifications/ColorToast.kt
@@ -1,13 +1,12 @@
 package za.co.xisystems.itis_rrm.custom.notifications
 
-import www.sanju.motiontoast.MotionToastStyle
 import za.co.xisystems.itis_rrm.custom.notifications.ToastDuration.SHORT
 import za.co.xisystems.itis_rrm.custom.notifications.ToastGravity.CENTER
 
 data class ColorToast(
     val title: String? = null,
     val message: String,
-    val style: MotionToastStyle = MotionToastStyle.INFO,
+    val style: ToastStyle = ToastStyle.INFO,
     val gravity: ToastGravity = CENTER,
     val duration: ToastDuration = SHORT
 )
