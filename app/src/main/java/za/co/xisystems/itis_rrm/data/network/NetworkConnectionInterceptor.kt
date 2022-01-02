bf1a95b8592f8d26d517eedc207d764294e53ed4
Shaun McDonald <shaun.mcdonald@xisystems.co.za>
2022-01-02 14:39:18 +0200

    ⚡️ MOBILEXI-1987 Improved network connection interceptor
    Signed-off-by: Shaun McDonald <shaun.mcdonald@xisystems.co.za>
    
    Took 1 hour 48 minutes

--- app/src/main/java/za/co/xisystems/itis_rrm/data/network/NetworkConnectionInterceptor.kt
+++ app/src/main/java/za/co/xisystems/itis_rrm/data/network/NetworkConnectionInterceptor.kt
@@ -1,15 +1,17 @@
 package za.co.xisystems.itis_rrm.data.network
 
 import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import java.io.IOException
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
 import okhttp3.Interceptor
 import okhttp3.Response
-import za.co.xisystems.itis_rrm.BuildConfig
+import timber.log.Timber
 import za.co.xisystems.itis_rrm.custom.errors.NoInternetException
 import za.co.xisystems.itis_rrm.custom.errors.ServiceHostUnreachableException
-import za.co.xisystems.itis_rrm.utils.ServiceUtil
-import java.io.IOException
-import java.net.HttpURLConnection
-import java.net.URL
+import za.co.xisystems.itis_rrm.utils.ServiceUriUtil
 
 /**
  * Created by Francis Mahlava on 2019/10/18.
@@ -17,45 +19,52 @@ import java.net.URL
 class NetworkConnectionInterceptor(
     context: Context
 ) : Interceptor {
-    private val testConnection = "8.8.8.8"
-    private val serviceHost = BuildConfig.API_HOST // ServiceUriUtil.getInstance()?.webServiceHost
+    private val serviceUriUtil = ServiceUriUtil.getInstance()!!
+    private val serviceURL = serviceUriUtil.webServiceHost!!
     private val applicationContext = context.applicationContext
-    override fun intercept(chain: Interceptor.Chain): Response {
 
-        if (!ServiceUtil.isNetworkAvailable(applicationContext)) {
-            throw NoInternetException("Please ensure you have an active data connection")
+    override fun intercept(chain: Interceptor.Chain): Response {
+        if (!isInternetAvailable()) {
+            throw NoInternetException("Make sure you have an active data connection")
         }
 
-//        if (isHostAvailable(testConnection)) {
-//            throw NoConnectivityException(
-//                "Network appears to be down, please try again later. Host: $testConnection"
-//            )
-//        }
-
-        if (isHostAvailable(serviceHost)) {
+        if (!isURLActive(URL(serviceURL))) {
             throw ServiceHostUnreachableException(
-                "Service Host for RRM is down, please try again later. Host: $serviceHost"
+                "Service Host for RRM is down, please try again later."
             )
         }
 
         return chain.proceed(chain.request())
     }
 
-    fun isHostAvailable(testURL: String): Boolean {
-//    , port = 443, timeout = 60_000L
-        return ping(url = URL(testURL))
+    private fun isInternetAvailable(): Boolean {
+        var result = false
+        val connectivityManager =
+            applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
+        connectivityManager?.let {
+            it.getNetworkCapabilities(connectivityManager.activeNetwork)?.apply {
+                result = when {
+                    hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                    hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                    hasTransport(NetworkCapabilities.TRANSPORT_VPN) -> true
+                    else -> false
+                }
+            }
+        }
+        return result
     }
 
-    fun ping(url: URL, timeoutMillis: Int = 50000): Boolean {
-        val connection = url.openConnection() as HttpURLConnection
+    private fun isURLActive(url: URL, timeoutMillis: Int = 60000): Boolean {
+        val connection = url.openConnection() as HttpsURLConnection
         return try {
             connection.connectTimeout = timeoutMillis
             connection.readTimeout = timeoutMillis
             connection.requestMethod = "HEAD"
+            connection.connect()
             val responseCode = connection.responseCode
-            connection.disconnect()
-            HttpURLConnection.HTTP_OK <= responseCode && responseCode < HttpURLConnection.HTTP_BAD_REQUEST
+            HttpsURLConnection.HTTP_OK <= responseCode && responseCode < HttpsURLConnection.HTTP_BAD_REQUEST
         } catch (exception: IOException) {
+            Timber.e("Could not connect to service host: ${exception.message}")
             false
         } finally {
             connection.disconnect()
